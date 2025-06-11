5fb891aac38215e42542b20b7bb31a9a42af83f7
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-11 19:21:00 +0100

    add: ui/button

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
+import Box from '@/components/ui/box'
 import Container from '@/components/ui/container'
-import { H1 } from '@/components/ui/typographie'
+import { H1, Span } from '@/components/ui/typographie'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 const index = () => {
-  return (
-    <Container >
-        <H1>Index</H1>
-    </Container>
-  )
+    return (
+        <Container >
+            <Box>
+                <H1>Welecome to MangoChat </H1>
+                <Span>is a chat app developed with react native and laravel</Span>
+                
+            </Box>
+        </Container>
+    )
 }
 
 export default index
---
+++ components/ui/button.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+type ButtonProps ={
+    children:ReactNode
+}
+
+const Button = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
