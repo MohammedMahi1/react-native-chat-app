6ad558487f6cc4549e6a8e6b2b6002af418c2f84
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-12 19:31:33 +0100

    fix: link/button-ui

--- app/index.tsx
+++ app/index.tsx
@@ -4,11 +4,10 @@ import Container from '@/components/ui/container'
 import { H1, Span } from '@/components/ui/typographie'
 import { Link } from 'expo-router'
 import React from 'react'
-
 const index = () => {
     return (
         <Container className='h-[100vh] flex items-center justify-center py-20'>
-            <Box className='items-center justify-between w-full h-full'>
+            <Box className='items-center justify-between gap-20 w-full h-full'>
                 <Box className='w-full gap-5'>
                     <H1 className='font-bold text-center text-6xl'>Welecome to MangoChat </H1>
                     <Span className='text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestias accusamus voluptates mollitia veritatis odit quia non nisi alias, similique beatae dolorem nulla harum quo cumque numquam illo blanditiis dolores.</Span>
@@ -16,15 +15,13 @@ const index = () => {
 
                 <Box className='items-center justify-center w-full gap-10'>
                     <Button>
-                        <Link href={"/auth/login"}>
-                            Get Started
-                        </Link>
+                        Get Started
                     </Button>
-                    <Button variant={"outline"}>
-                        <Link href={"/auth"}>
+                    <Link href='/auth/login' asChild>
+                        <Button variant={"outline"} >
                             Add an existing account
-                        </Link>
-                    </Button>
+                        </Button>
+                    </Link>
                 </Box>
             </Box>
         </Container>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,6 +4,7 @@ import React, { ReactNode } from 'react'
 import { ButtonProps, Pressable, PressableProps, Text, useColorScheme, ViewProps } from 'react-native'
 import { Label } from './typographie'
 import { twMerge } from 'tailwind-merge'
+import { Link, LinkProps } from 'expo-router'
 
 
 
@@ -24,21 +25,20 @@ const variants = cva(
                 outline: ["border-black dark:border-white", "border-2 bg-transparent"]
             },
         },
-        defaultVariants:{
-            variant:"contain",
+        defaultVariants: {
+            variant: "contain",
         }
     }
 )
 
 type UiButtonProps = {
-    children: ReactNode
+    children: ReactNode;
 } & PressableProps & VariantProps<typeof variants>
 
-const Button = ({ variant,children, ...rest }: UiButtonProps) => {
-    const theme = useColorScheme()
+const Button = ({ variant, children, ...rest }: UiButtonProps) => {
     return (
-        <Pressable className={cn(variants({variant}))}>
-            <Text className={twMerge(variant === "contain" || variant === undefined ? "dark:text-black text-white":"dark:text-white text-black","text-2xl font-semibold")}>
+        <Pressable className={cn(variants({ variant }))} {...rest}>
+            <Text className={twMerge(variant === "contain" || variant === undefined ? "dark:text-black text-white" : "dark:text-white text-black", "text-2xl font-semibold")}>
                 {children}
             </Text>
         </Pressable>
