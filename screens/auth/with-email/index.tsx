c37063d1cf5b5429654bd30d4282812ed09a8812
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-16 20:57:46 +0100

    Refactor OnBoard screen and enhance IconTip integration for improved user experience
    - Updated OnBoard screen layout for better visual consistency and interaction.
    - Enhanced IconTip component functionality and styling for improved alignment.
    - Integrated updated IconTip into OnBoard screen to streamline user interactions.

---
+++ screens/auth/with-email/index.tsx
@@ -0,0 +1,13 @@
+import Container from 'components/Container'
+import { T } from 'components/ui/Typographies'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Container>
+      <T.H1>Sign up with Email</T.H1>
+    </Container>
+  )
+}
+
+export default index
