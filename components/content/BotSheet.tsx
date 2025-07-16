33b1985e26529b895c6eb362766b1907e9436cba
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-16 17:28:11 +0100

    Refactor OnBoard screen and enhance Button component for improved theming
    - Updated OnBoard screen to utilize the new Button component features, including dynamic theming.
    - Enhanced Button component to support additional styling options and improved variant handling.
    - Adjusted layout and styling in OnBoard screen for better visual consistency and user experience.

---
+++ components/content/BotSheet.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const BotSheet = () => {
+  return (
+    <View>
+        <Text>BotSheet</Text>
+    </View>
+  )
+}
+
+export default BotSheet
