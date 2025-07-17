2648090824771cb88b93e7d6ed62971ae1849f1e
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-17 19:46:55 +0100

    Enhance BottomModal component with improved visibility and state management
    - Refactored BottomModal to include a backdrop and handle modal visibility based on Redux state.
    - Added onRequestClose and onDismiss handlers for better user interaction.
    - Updated styling for the modal and backdrop to enhance user experience.

--- components/content/BottomModal.tsx
+++ components/content/BottomModal.tsx
@@ -1,30 +1,37 @@
 import React from 'react'
-import { Modal, ModalProps, SafeAreaView, Text, useColorScheme, View } from 'react-native'
+import { Modal, ModalProps, SafeAreaView, Text, TouchableWithoutFeedback, useColorScheme, View } from 'react-native'
 import * as NavigationBar from 'expo-navigation-bar';
 import { useAppDispatch, useAppSelector } from 'hooks/hook';
+import { closeModal } from 'libs/redux-toolkit/modalSlice';
 
 type BottomModalProps = ModalProps & {
     children: React.ReactNode;
     id: string;
 }
 
-const BottomModal = ({ children,id, ...props }: BottomModalProps) => {
+const BottomModal = ({ children, id, ...props }: BottomModalProps) => {
     const scheme = useColorScheme();
     const dispatch = useAppDispatch();
     const isOpen = useAppSelector((state) => state.modal.modals[id]);
 
     return (
-        <Modal
-            animationType="slide"
-            {...props}
-            transparent={true}
-            statusBarTranslucent={true}
-            navigationBarTranslucent={true}
-        >
-            <View style={{ padding:24,justifyContent: 'flex-end',width:"100%",flex: 1 }}>
-                {children}
-            </View>
-        </Modal>
+        <View style={{ flex: 1, backgroundColor: "#00000050",position:"absolute",top:0,left:0,right:0,bottom:0,zIndex:1000 }}>
+            <Modal
+                animationType="slide"
+                visible={isOpen}
+                transparent={true}
+                onRequestClose={() => dispatch(closeModal(id))}
+                onDismiss={() => console.log("dismissed")}
+                statusBarTranslucent={true}
+                navigationBarTranslucent={true}
+                {...props}
+            >
+
+                <View style={{ padding: 24, justifyContent: 'flex-end', width: "100%", flex: 1 }} >
+                    {children}
+                </View>
+            </Modal>
+        </View>
     )
 }
 
