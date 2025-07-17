6cd0e5450956346e938e0d88dbf630467e993ffb
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-07-17 10:38:29 +0100

    Refactor ScreensSwipie component and enhance EmailScreen layout
    - Updated ScreensSwipie to include optional goBack and iconed props, improving flexibility.
    - Replaced footer with a GitHub profile link in ScreensSwipie for better user engagement.
    - Modularized EmailScreen by creating HeaderEmail and BodyEmail components for improved readability and maintainability.

--- components/content/ScreensSwipie.tsx
+++ components/content/ScreensSwipie.tsx
@@ -1,33 +1,54 @@
 import { FontAwesome6 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import Link from 'components/Link';
+import { T } from 'components/ui/Typographies';
 import React from 'react'
-import { Pressable, useColorScheme, View } from 'react-native'
+import { Image, Pressable,  useColorScheme, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type ScreensSwipieProps = {
-    header:React.ReactNode;
-    footer:React.ReactNode;
-    body:React.ReactNode;
+    header: React.ReactNode;
+    body: React.ReactNode;
+    goBack?: boolean;
+    iconed?: boolean;
 }
 
-const ScreensSwipie = ({header,footer,body}:ScreensSwipieProps) => {
+const ScreensSwipie = ({ header, body, goBack = true, iconed = true }: ScreensSwipieProps) => {
     const navigation = useNavigation();
     const theme = useColorScheme();
-  return (
-    <View style={{flex:1,justifyContent:"space-between",alignItems:"flex-start"}}>
-            <Pressable onPress={()=>navigation.goBack()} >
-                <FontAwesome6 name="arrow-left-long" size={32} color={theme === 'dark' ? '#ffffff' : '#0A0A0A'} />
-            </Pressable>
-        <View>
-            {header}
-        </View>
-        <View>
-            {body}
-        </View>
-        <View>
-            {footer}
-        </View>
-    </View>
-  )
+    return (
+        <SafeAreaView style={{ flex: 1, justifyContent: "space-between", alignItems: "flex-start" }}>
+
+            <View style={{ gap: 24 }}>
+                {
+                    goBack && (
+                        <View>
+                            <Pressable onPress={() => navigation.goBack()} >
+                                <FontAwesome6 name="arrow-left-long" size={32} color={theme === 'dark' ? '#ffffff' : '#0A0A0A'} />
+                            </Pressable>
+                        </View>
+                    )
+                }
+                {
+                    iconed && theme === "dark" ?
+                        <Image source={require("../../assets/SwipletLogoDark.png")} /> :
+                        <Image source={require("../../assets/SwipletLogoLight.png")} />
+                }
+                <View style={{ gap: 24 }}>
+                    {header}
+                </View>
+                <View style={{ gap: 24 }}>
+                    {body}
+                </View>
+            </View>
+            <View style={{ alignItems: "center" }}>
+                <T>
+                    Hi I’m Mohammed Mahi, Checkout my profile on GitHub,
+                </T>
+                <Link href="https://github.com/MohammedMahi1">MohammedMahi1 </Link>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default ScreensSwipie
--- screens/auth/with-email/EmailScreen.tsx
+++ screens/auth/with-email/EmailScreen.tsx
@@ -1,5 +1,6 @@
 import Container from 'components/Container'
 import ScreensSwipie from 'components/content/ScreensSwipie'
+import Button from 'components/ui/Button'
 import { T } from 'components/ui/Typographies'
 import React from 'react'
 import { View } from 'react-native'
@@ -19,15 +20,30 @@ export const EmailScreenPart = () => {
 
 
 
-
-
+const HeaderEmail = () => {
+  return (
+    <>
+      <T.H1>Welcome to Swiplete</T.H1>
+      <View>
+        <T.Span>Your world of chats starts here.</T.Span>
+        <T.Span>Enter your email to dive in.</T.Span>
+      </View>
+    </>
+  )
+}
+const BodyEmail = () => {
+  return (
+    <>
+      <Button >Continue</Button>
+    </>
+  )
+}
 export const EmailScreen = () => {
   return (
     <Container>
       <ScreensSwipie
-        header={<T.H1>Sign up with Email</T.H1>}
-        footer={<T.H1>Sign up with Email</T.H1>}
-        body={<T.H1>Sign up with Email</T.H1>} />
+        header={<HeaderEmail />}
+        body={<BodyEmail />} />
     </Container>
   )
 }
